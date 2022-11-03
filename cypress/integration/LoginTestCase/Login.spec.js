b66a47481e4d927afa6fd8d84f6986db805a1a58
ajit2599 <godeajit71@gmail.com>
2022-11-03 19:07:00 +0530

    text

--- cypress/integration/LoginTestCase/Login.spec.js
+++ cypress/integration/LoginTestCase/Login.spec.js
@@ -5,13 +5,13 @@
 
 describe("verify the login functionality",()=>{
     
-    // it("Verify the login functionality with valid credential",()=>{
-    //     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    //     cy.get('input[name="username"]').type('Admin')
-    //     cy.get('input[name="password"]').type('admin123')
-    //     cy.get('.oxd-button').click()
-    //     cy.get('img[alt="client brand banner"]').should('be.visible')
-    // })
+    it("Verify the login functionality with valid credential",()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('input[name="username"]').type('Admin')
+        cy.get('input[name="password"]').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.get('img[alt="client brand banner"]').should('be.visible')
+    })
 
     it("Verify the login functionality with invalid credential",()=>{
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
