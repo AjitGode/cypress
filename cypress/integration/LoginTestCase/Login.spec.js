29ef723095858713781bf7886fbb4503d2a74679
ajit2599 <godeajit71@gmail.com>
2022-11-03 18:32:24 +0530

    text

---
+++ cypress/integration/LoginTestCase/Login.spec.js
@@ -0,0 +1,23 @@
+// javascript is object oriended language
+
+// cy ---- cy is object and object has property and methods
+
+
+describe("verify the login functionality",()=>{
+    
+    // it("Verify the login functionality with valid credential",()=>{
+    //     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    //     cy.get('input[name="username"]').type('Admin')
+    //     cy.get('input[name="password"]').type('admin123')
+    //     cy.get('.oxd-button').click()
+    //     cy.get('img[alt="client brand banner"]').should('be.visible')
+    // })
+
+    it("Verify the login functionality with invalid credential",()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('input[name="username"]').type('Admin')
+        cy.get('input[name="password"]').type('admin1234')
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-alert-content-text').should('have.text','Invalid credentials')
+    })
+})
